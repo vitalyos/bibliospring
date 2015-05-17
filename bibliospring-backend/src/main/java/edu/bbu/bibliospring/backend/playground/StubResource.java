41d2f2dc89bc695dd7338bb50fec66316d0d7697
zsvitalyos <zsolt.vitalyos@gmail.com>
2015-05-17 20:19:55 +0300

    Licence header templetek kiszedve

--- bibliospring-backend/src/main/java/edu/bbu/bibliospring/backend/playground/StubResource.java
+++ bibliospring-backend/src/main/java/edu/bbu/bibliospring/backend/playground/StubResource.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.bbu.bibliospring.backend.playground;
 
 import javax.ejb.LocalBean;
